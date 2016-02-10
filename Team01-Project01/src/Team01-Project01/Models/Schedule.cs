8095924370e139f644c6855a6084db77021567c7
s525248@mail.nwmissouri.edu <s525248@mail.nwmissouri.edu>
2016-02-09 22:43:15 -0600

    Manindra Commit

--- Team01-Project01/src/Team01-Project01/Models/Schedule.cs
+++ Team01-Project01/src/Team01-Project01/Models/Schedule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,7 @@ namespace Team01_Project01.Models
 {
     public class Schedule
     {
-[ScaffoldColumn(false)]
+            [ScaffoldColumn(false)]
             public int ScheduleId { get; set; }
             [Required]
             [Display(Name = "Faculty Available")]
