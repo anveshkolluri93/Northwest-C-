cf456d7b21d7bc71fad7fea453811911bdb651e2
Jagadeesh Babu Basimsetty <jagadeesh.basimsetty@gmail.coim>
2016-02-09 21:32:55 -0600

    model commit

--- Team01-Project01/src/Team01-Project01/Models/Section.cs
+++ Team01-Project01/src/Team01-Project01/Models/Section.cs
@@ -2,19 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Team01_Project01.Models
 {
+
     public class Section
     {
+
         [ScaffoldColumn(false)]
         public int SectionId { get; set; }
-        [Required]
-        [Display(Name = "Room")]
-        public string roomNumber { get; set; }
+
+        [Range(0,50)]
+        [Display(Name ="Capacity")]
+        public int capacity { get; set;  }
 
         [Display(Name = "Faculty Name")]
         public string FacultyId { get; set; }
 
+        [Required]
+        [Display(Name = "Room")]
+        public string roomNumber { get; set; }
     }
 }
