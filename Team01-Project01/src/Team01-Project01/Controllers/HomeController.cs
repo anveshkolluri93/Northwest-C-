af1d440046498f5fd1a78d2fa393281b9c83b4fe
s525248@mail.nwmissouri.edu <s525248@mail.nwmissouri.edu>
2016-02-20 15:57:14 -0600

    UI_commit

--- Team01-Project01/src/Team01-Project01/Controllers/HomeController.cs
+++ Team01-Project01/src/Team01-Project01/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Team01_Project01.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Edit Team01 Description here";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Edit Team01  contact details.";
 
             return View();
         }
