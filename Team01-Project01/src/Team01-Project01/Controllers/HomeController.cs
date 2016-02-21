3c8e09c002ef9d5d96fed138188892ceff205e62
Jagadeesh Babu Basimsetty <jagadeesh.basimsetty@gmail.coim>
2016-02-21 08:48:30 -0600

    Initial views commit

--- Team01-Project01/src/Team01-Project01/Controllers/HomeController.cs
+++ Team01-Project01/src/Team01-Project01/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Team01_Project01.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+           // ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            //ViewData["Message"] = "Your contact page.";
 
             return View();
         }
