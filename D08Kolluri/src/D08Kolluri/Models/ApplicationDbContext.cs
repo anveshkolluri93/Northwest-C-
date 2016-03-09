3a7a2eee8f545bed0040e6feb127c2fe4fa83f24
anvesh <s525272@mail.nwmissouri.edu>
2016-03-09 13:15:26 -0600

    D09 pull

---
+++ D08Kolluri/src/D08Kolluri/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+
+namespace D08Kolluri.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
