e89664269a76d3a4fe34362e6462eb43fd867827
krishnapainuly <S525274@mail.nwmissori.edu>
2016-03-18 14:16:54 -0500

    Reverted till P01 final commmit
    Anvesh

--- DataAnveshkumarKolluri/src/DataAnveshkumarKolluri/Models/AppSeedData.cs
+++ DataAnveshkumarKolluri/src/DataAnveshkumarKolluri/Models/AppSeedData.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using DataAnveshkumarKolluri.Models;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DataAnveshkumarKolluri.Models
--- SampleDummy/src/SampleDummy/Models/ApplicationDbContext.cs
+++ SampleDummy/src/SampleDummy/Models/ApplicationDbContext.cs
@@ -18,5 +18,6 @@ namespace SampleDummy.Models
             // Add your customizations after calling base.OnModelCreating(builder);
         }
         public DbSet<dummy> dummy { get; set; }
+        public DbSet<Actor> Actor { get; set; }
     }
 }
