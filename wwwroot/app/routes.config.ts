97765bce85867174fc58b9be025f1af4bf88f7d0
anvesh <s525272@mail.nwmissouri.edu>
2016-03-17 23:33:34 -0500

    check commit

--- wwwroot/app/routes.config.ts
+++ wwwroot/app/routes.config.ts
@@ -3,12 +3,17 @@ import { About } from './about/About';
 import { People } from './people/People';
 import { PersonDetail } from './people/PersonDetail';
 import { Route, Router } from 'angular2/router';
+import {My} from './my/My';
+import {Anvesh} from './anvesh/Anvesh';
 
 export var Routes = {
   home: new Route({path: '/', as: 'Home', component: Home}),
   about: new Route({path: '/about', as: 'About', component: About}),
   people: new Route({ path: '/people', as: 'People', component: People }),
-  detail: new Route({path: '/people/:id', as: 'Detail', component: PersonDetail })
+  my: new Route({ path: '/my', as: 'My', component: My }),
+  anvesh: new Route({ path: '/anvesh', as: 'Anvesh', component: Anvesh  }),
+  detail: new Route({ path: '/people/:id', as: 'Detail', component: PersonDetail })
+
 };
 
 export const APP_ROUTES = Object.keys(Routes).map(r => Routes[r]);
