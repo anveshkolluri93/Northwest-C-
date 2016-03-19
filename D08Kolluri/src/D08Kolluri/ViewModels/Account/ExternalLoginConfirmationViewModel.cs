6a0e3e0bae0e071dcfcc334729f4ae72448cef98
krishnapainuly <S525274@mail.nwmissori.edu>
2016-03-19 00:35:29 -0500

    Reverted back the recent changes that affected the main project.

---
+++ D08Kolluri/src/D08Kolluri/ViewModels/Account/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D08Kolluri.ViewModels.Account
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
